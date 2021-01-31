1dc9e0df27ca469fd42190e190e30b8898cf3546
marilourenco <lourencomarilene@gmail.com>
2021-01-30 22:36:50 -0300

    creating all settings for the cypress project and the first test

---
+++ cypress/integration/tickets.spec.js
@@ -0,0 +1,8 @@
+///<reference types = "cypress"/>
+
+describe('Tickets', () => {
+  beforeEach(() => cy.visit('https://bit.ly/2XSuwCW'));
+  it('fills all the text input fields', () => {});
+
+  it("has 'TICKETBOX' header's heading", () => {});
+});
